01c53527aa5ad496082dfc3472c6eca511ee76fa
Виктор Парахин <victor1810@vip-pc.localdomain>
2022-08-28 21:45:43 +0300

    dzOneNumFour is done

--- HomeworkOne/Program.cs
+++ HomeworkOne/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая на вход принимает число (N),
+а на выходе показывает все чётные числа от 1 до N */
+
+int num = new Random().Next(1, 100);
+int index = 1;
+
+Console.Write($"N = {num}:  ");
+
+while ((index - 1) < num)
+{
+    if (index % 2 == 0)
+        Console.Write($"{index}, ");
+    index++;
+}
