8df045f9b6733014a0c81babe31d295e18caf4b1
Виктор Парахин <victor1810@vip-pc.localdomain>
2022-08-28 21:43:47 +0300

    dzOneNumThree is done

--- HomeworkOne/Program.cs
+++ HomeworkOne/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая на вход принимает число и выдаёт,
+является ли число чётным (делится ли оно на два без остатка). */
+
+int a = new Random().Next(1, 100);
+
+if (a % 2 == 0)
+    Console.Write($"{a} -> это число чётное");
+else
+    Console.Write($"{a} -> это число нечётное");
