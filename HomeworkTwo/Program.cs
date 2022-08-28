99b4b9a02a7d8da8e98459ae2b596c09d347a8f2
Виктор Парахин <victor1810@vip-pc.localdomain>
2022-08-28 22:09:30 +0300

    dzTwoNumThree is done

--- HomeworkTwo/Program.cs
+++ HomeworkTwo/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая принимает на вход цифру,
+обозначающую день недели, и проверяет, является ли этот день выходным. */
+
+int number = new Random().Next(1, 8);
+
+if (number == 6 || number == 7)
+    Console.WriteLine($"{number} -> выходной");
+else
+    Console.WriteLine($"{number} -> будний");
