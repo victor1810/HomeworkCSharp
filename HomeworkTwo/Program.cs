b97160b23e3c1ab391d56a024580351a6e4d4ba4
Виктор Парахин <victor1810@vip-pc.localdomain>
2022-08-28 22:06:02 +0300

    dzTwoNumTwo is done

--- HomeworkTwo/Program.cs
+++ HomeworkTwo/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая выводит третью цифру
+заданного числа или сообщает, что третьей цифры нет. */
+
+int number = new Random().Next(10, 100000);
+int s = number.ToString().Length;
+
+int DigitOfNumber(int num, int n)
+{
+    return num / (int)Math.Pow(10, s - n) % 10;
+}
+if (s <= 2)
+    Console.Write($"{number} -> третьей цифры нет");
+else
+    Console.Write($"{number} -> {DigitOfNumber(number, 3)}");
