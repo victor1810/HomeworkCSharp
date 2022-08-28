714461ce29274d6c008447491df49f84c012f851
Виктор Парахин <victor1810@vip-pc.localdomain>
2022-08-28 21:34:50 +0300

    dzOneNumTwo is done

--- HomeworkOne/Program.cs
+++ HomeworkOne/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая принимает на вход три числа
+и выдаёт максимальное из этих чисел. */
+
+int a = new Random().Next(1, 100);
+int b = new Random().Next(1, 100);
+int c = new Random().Next(1, 100);
+
+int max = a;
+
+if (b > max)
+    max = b;
+if (c > max)
+    max = c;
+Console.Write($"a = {a}, b = {b}, c = {c}. max ->  {max}");
