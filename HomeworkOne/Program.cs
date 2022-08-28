c7ce01166524b897c29b13b886db78d48fa39835
Виктор Парахин <victor1810@vip-pc.localdomain>
2022-08-28 21:27:16 +0300

    dzOneNumOne is done

--- HomeworkOne/Program.cs
+++ HomeworkOne/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая на вход принимает два числа
+и выдаёт, какое число большее, а какое меньшее. */
+
+int a = new Random().Next(1, 1000);
+int b = new Random().Next(1, 1000);
+
+if (a > b)
+    Console.Write($"a = {a}; b = {b} -> max = {a}");
+else
+    Console.Write($"a = {a}; b = {b} -> max = {b}");
