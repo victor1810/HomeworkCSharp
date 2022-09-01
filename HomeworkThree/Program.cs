6837e18cf9fad3522b3ebc6d6bef99a9b09f571c
victor1810 <victor181078@gmail.com>
2022-09-01 16:02:36 +0300

    dzThreeNumOne

--- HomeworkThree/Program.cs
+++ HomeworkThree/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая принимает на вход пятизначное
+число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+12821 -> да
+23432 -> да */
+
+Console.Write("Введите пятизначное число: ");
+string? number = Console.ReadLine();
+
+void PalindromeNumber(string number)
+{
+    if (number[0] == number[4] || number[1] == number[3])
+    {
+        Console.WriteLine($"Число {number} - палиндром.");
+    }
+    else Console.WriteLine($"Число {number} - не является палиндромом.");
+}
+
+PalindromeNumber(number);
