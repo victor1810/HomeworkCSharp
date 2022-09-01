2f1b1c3061975a8fa2b879f84099869d03921f42
victor1810 <victor181078@gmail.com>
2022-09-01 19:21:44 +0300

    dzThreeMunThree

--- HomeworkThree/Program.cs
+++ HomeworkThree/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125          */
+
+int num = new Random().Next(10);
+Console.Write($"{num} -> ");
+for (int i = 1; i <= num; i++) Console.Write($"{i * i * i} ");
