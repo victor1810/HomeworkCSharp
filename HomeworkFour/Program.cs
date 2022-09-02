c5b52ccc7e0c5b44dd383b31d460e32fe5150cf9
victor1810 <victor181078@gmail.com>
2022-09-02 09:31:16 +0300

    dzFourNumOne

--- HomeworkFour/Program.cs
+++ HomeworkFour/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите цикл, который принимает на вход два числа (A и B)
+и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+2, 4 -> 16                                                 */
+
+int A = new Random().Next(10);
+int B = new Random().Next(5);
+
+int power = 1;
+for (int i = 1; i <= B; i++)
+{
+    power *= A;
+}
+
+Console.WriteLine($"A = {A}; B = {B} -> {power}");
