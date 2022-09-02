0b58a63888dbb7b6718cdb117b7883a96d3b75d9
victor1810 <victor181078@gmail.com>
2022-09-02 16:55:36 +0300

    dzFiveNumThree

--- HomeworkFive/Program.cs
+++ HomeworkFive/Program.cs
@@ -1,13 +1,23 @@
-﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
-Напишите программу, которая покажет количество чётных чисел в массиве.
+﻿/* Задача 38: Задайте массив вещественных чисел.
+Найдите разницу между максимальным и минимальным элементов массива.
 
-[345, 897, 568, 234] -> 2                           */
+[3 7 22 2 78] -> 76                                             */
 
-int[] array = { new Random().Next(1000), new Random().Next(1000), new Random().Next(1000), new Random().Next(1000) };
-int j = 0;
+int[] array = { new Random().Next(-99, 100), new Random().Next(-99, 100), new Random().Next(-99, 100), new Random().Next(-99, 100), new Random().Next(-99, 100) };
 for (int i = 0; i < array.Length; i++)
-    if (array[i] % 2 == 0)
-        j += 1;
+    array[i] = new Random().Next(1, 10);
 
+int max = array[0];
+int min = array[0];
 
-Console.Write($"[{array[0]}, {array[1]}, {array[2]}, {array[3]}] -> {j}");
+for (int i = 1; i < array.Length; i++)
+{
+    if (max < array[i])
+        max = array[i];
+
+    if (min > array[i])
+        min = array[i];
+}
+int diff = max - min;
+Console.Write($"[{array[0]}, {array[1]}, {array[2]}, {array[3]}, {array[4]}] ");
+Console.WriteLine($"-> max({max}) - min({min}) = {diff}");
