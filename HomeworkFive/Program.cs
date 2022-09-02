64f895e800abd662ff92dd1d41ac447e5d521d30
victor1810 <victor181078@gmail.com>
2022-09-02 15:36:13 +0300

    change main

--- HomeworkFive/Program.cs
+++ HomeworkFive/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2                           */
+
+int[] array = { new Random().Next(1000), new Random().Next(1000), new Random().Next(1000), new Random().Next(1000) };
+int j = 0;
+for (int i = 0; i < array.Length; i++)
+    if (array[i] % 2 == 0)
+        j += 1;
+
+
+Console.Write($"[{array[0]}, {array[1]}, {array[2]}, {array[3]}] -> {j}");
