2592ac7c0901b8e323ec0bca5c44cd2f956cdace
victor1810 <victor181078@gmail.com>
2022-09-02 10:25:27 +0300

    dzFourNumTwo

--- HomeworkFour/Program.cs
+++ HomeworkFour/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 27: Напишите программу, которая принимает
+на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+82 -> 10
+9012 -> 12                                       */
+
+int num = new Random().Next(10000);
+
+int SumNum(int num)
+{
+    int count = Convert.ToString(num).Length;
+    int step = 0;
+    int sum = 0;
+
+    for (int i = 0; i < count; i++)
+    {
+        step = num - num % 10;
+        sum = sum + (num - step);
+        num = num / 10;
+    }
+    return sum;
+}
+Console.WriteLine($"{num} -> {SumNum(num)}");
