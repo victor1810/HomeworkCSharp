9703ce95f158d628f5d41705037ef63fe5072572
victor1810 <victor181078@gmail.com>
2022-09-06 11:39:35 +0300

    dzSixNumOne

--- HomeworkSix/Program.cs
+++ HomeworkSix/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел.
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3                         */
+
+Console.Write("Введите элементы через пробел: ");
+int[] array = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Элементов > 0: -> {count}");
