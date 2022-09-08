2dcc953b9025f3e8d380aa6bd837fdb99d17dd2e
victor1810 <victor181078@gmail.com>
2022-09-08 19:43:03 +0300

    dzEightNumOne

---
+++ HomeworkEight/Program.cs
@@ -0,0 +1,56 @@
+﻿/* Задача 54: Задайте двумерный массив. Напишите программу, которая
+упорядочит по убыванию элементы каждой строки двумерного массива. */
+
+Console.WriteLine($"\nНе отсортированный массив:");
+int m = new Random().Next(3, 10);
+int n = new Random().Next(3, 10);
+
+int[,] array = new int[m, n];
+CreateArray(array);
+PrintArray(array);
+
+Console.WriteLine($"\nОтсортированный массив:");
+SortArray(array);
+PrintArray(array);
+
+void CreateArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 100);
+        }
+    }
+}
+
+void SortArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
