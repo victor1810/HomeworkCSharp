d4917c58df63bc0f31824ba0e142ec04ed5f1de6
victor1810 <victor181078@gmail.com>
2022-09-09 16:56:35 +0300

    dzNineNumOne

--- HomeworkNine/Program.cs
+++ HomeworkNine/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 64: Задайте значение N. Напишите программу,
+которая выведет все натуральные числа в промежутке от N до 1.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"                     */
+
+int n = new Random().Next(1, 25);
+Console.WriteLine($"N = {n}\n");
+
+for (int i = 0; i < n; n -= 1)
+{
+    Console.Write($"{n} ");
+}
